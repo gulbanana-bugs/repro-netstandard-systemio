c17a60084c21d9f87b00ee5d96dc367d7d7fd605
Thomas Castiglione <thomas@agcom.com.au>
2016-12-16 19:25:17 +0800

    reproduce scenario in a unit test

---
+++ netfxtest/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace netfxtest
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            var x = new netstdlib.Class1();
+        }
+    }
+}
--- netstdlib/Class1.cs
+++ netstdlib/Class1.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace netstdlib
 {
     public class Class1
     {
+        public Class1()
+        {
+            using (var f = File.OpenRead("C:\\Windows\\win.ini"))
+            {
+                using (var r = new StreamReader(f))
+                {
+                    Debug.WriteLine(r.ReadToEnd());
+                }
+            }
+        }
     }
 }
